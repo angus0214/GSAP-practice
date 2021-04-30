2cf01b4fa55a7d78bba6501688f22da7a7c2ca40
Angus Wu <s0937196597@gmail.com>
2021-04-30 17:58:46 +0800

    modify SVG import method

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-console.clear();
+// console.clear();
 
-let counter = {score: 0};
-const scoreDom = document.querySelector('.score');
-const tween = gsap.to(counter, {
-  duration: 10,
-  score: 100,
-  onUpdate() {
-    scoreDom.innerHTML = "time : " + counter.score.toFixed(2);
-  },
-});
+// let counter = {score: 0};
+// const scoreDom = document.querySelector('.score');
+// const tween = gsap.to(counter, {
+//   duration: 10,
+//   score: 100,
+//   onUpdate() {
+//     scoreDom.innerHTML = "time : " + counter.score.toFixed(2);
+//   },
+// });
