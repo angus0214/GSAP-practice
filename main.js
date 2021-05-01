5a0750ea12c8533e8c3b49650ac82f37ba10f1e8
Angus Wu <s0937196597@gmail.com>
2021-05-01 18:08:14 +0800

    control hands and simple animate

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// console.clear();
+console.clear();
 
 // let counter = {score: 0};
 // const scoreDom = document.querySelector('.score');
@@ -9,3 +9,21 @@
 //     scoreDom.innerHTML = "time : " + counter.score.toFixed(2);
 //   },
 // });
+const svg = document.querySelector('#queen')
+svg.addEventListener('load',()=>{
+    const svgDoc = svg.contentDocument;
+    const leftHand = svgDoc.querySelector('#圖層_2 > g > g >:nth-child(1)');
+    const rightHand = svgDoc.querySelector('#圖層_2 > g > g >:nth-child(2)');
+    // console.log(leftHand)
+    const tl = gsap.timeline()
+    tl
+    .to(leftHand,{
+        duration: 1,
+        y: -10,
+        x:100
+    })
+    .to(rightHand,{
+        duration: 1,
+        rotate:410,
+    })
+})
